7e338aeabda1147b4b44f2191c95e024bc265d9d
gitallstara <avr2820@gmail.com>
2024-02-27 01:53:54 -0600

    format

--- app/mvoingBorderDemo.tsx
+++ app/mvoingBorderDemo.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import { Button } from "../app/components/ui/moving-border";
 
-export function MovingBorderDemo({str}:{str:string}) {
+export function MovingBorderDemo({ str }: { str: string }) {
   return (
     <div>
       <Button
         borderRadius="1.75rem"
         className="w-full bg-white dark:bg-slate-900 bg-opacity-50 dark:bg-opacity-50 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-        >
+      >
         {str}
       </Button>
     </div>
