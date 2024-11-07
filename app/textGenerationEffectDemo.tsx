9ec9159cfe7c5cf47354f31ec6c32ecae8732f15
Vikranth_R_A <37049888+GitAllStarA@users.noreply.github.com>
2024-11-07 02:21:27 -0600

    Update textGenerationEffectDemo.tsx
    ui content update

--- app/textGenerationEffectDemo.tsx
+++ app/textGenerationEffectDemo.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "../app/components/ui/text-generate-effect";
 
-// const words = `Hello there! I'm a Motivated Software Engineer and AI/ML Enthusiast and I work on JSP, React, Angular, Lightning Web Components, MuleSoft and  Java Springboot Programming Technologies, seeking a challenging role to leverage my expertise in Web services, software design, cloud computing, and troubleshooting within a dynamic and collaborative team.`;
+// const words = `Hello there! I'm a Motivated Software Engineer and AI/ML Enthusiast and I work on JSP, React, Angular, Lightning Web Components, MuleSoft, Spring and Java Programming Technologies, seeking a challenging role to leverage my expertise in Web services, software design, cloud computing, and troubleshooting within a dynamic and collaborative team.`;
 // const word2 = `As always Jack of all trades, Master of none`;
 
 export function TextGenerateEffectDemo({ words }: { words: string }) {
